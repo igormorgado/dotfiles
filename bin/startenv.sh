78eab3b522728734b29621194d8ee0198732920b
igormorgado <igormorgado@users.noreply.github.com>
2016-07-31 10:10:26 +0000

    Created env initializer

---
+++ bin/startenv.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Install packages
+PACKAGES="
+vim
+mc
+exuberant-ctags
+git
+build-essential
+"
+sudo apt-get update
+sudo apt-get -y -f --force-yes install ${PACKAGES}
+
+
+# git clone dotfiles at $HOME
+if [ ! -d "${HOME}/dotfiles" ]; then
+	pushd ${HOME}
+	git clone https://github.com/igormorgado/dotfiles
+	popd
+fi
+
+# dotinst bash vim
+pushd ${HOME}/dotfiles
+./dotinst env
+./dotinst bash
+./dotinst vim
+popd
+
+# Vim install plugins
+vim -c ":PlugInstall | q"
+
+# Load congfigs
+source ${HOME}/.bashrc
+
