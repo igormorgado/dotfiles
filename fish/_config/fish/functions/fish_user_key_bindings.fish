c453b5aa739d23fd2afdbd79e2ca44644b52801d
Igor Morgado <morgado.igor@gmail.com>
2021-08-25 20:48:57 -0300

    Fish function and makefile

--- fish/_config/fish/functions/fish_user_key_bindings.fish
+++ fish/_config/fish/functions/fish_user_key_bindings.fish
@@ -1,5 +1,4 @@
 if status --is-interactive
-    # source $HOME/.config/fish/interactive.fish
     function fish_user_key_bindings --description 'Terminal bindings for interactive shell'
        bind \co 'clear; commandline -f repaint'
        bind --mode insert \co 'clear; commandline -f repaint'
