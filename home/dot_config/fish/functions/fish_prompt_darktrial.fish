1b4d18d61f5c823de0754d8b9094a497944e195f
Igor Morgado <morgado.igor@gmail.com>
2025-04-23 21:00:15 -0300

    Update home/.config/fish/functions/fish_prompt_darktrial.fish

--- home/dot_config/fish/functions/fish_prompt_darktrial.fish
+++ home/dot_config/fish/functions/fish_prompt_darktrial.fish
@@ -56,7 +56,10 @@ function __dt_suffix --description 'Write the prompt'
     functions -q fish_is_root_user; and fish_is_root_user; and set suffix '#'
 
     set -q fish_color_prompt; and set_color $fish_color_prompt
-    test "$last_status" != "0"; and set -q fish_color_error; and set_color $fish_color_error
+
+    test "$last_status" != "0";
+        and set -q fish_color_error;
+        and set_color $fish_color_error
 
     echo -n "$suffix"
     set_color normal
@@ -67,34 +70,25 @@ function __dt_login --description 'Write the login information'
 end
  
 function __dt_vcs_prompt --description 'Write vsc prompt'
-    if set -q fish_vcs_color
-        set_color $fish_vcs_color
-    end
+    set -q fish_vcs_color; and set_color $fish_vcs_color
     echo -n (fish_vcs_prompt)
     set_color normal
 end
    
 function __dt_prompt --description 'Write the prompt'
-    set -l last_status
-    if test (count $argv) -gt 0
-        set last_status $argv[1]
-    else
-        set last_status 0
-    end
-
+    set -q argv[1]; and set -l last_status $argv[1]; or set -l last_status 0
     echo -s -n (__dt_login) ':' (__dt_pwd) ' ' (__dt_vcs_prompt) ' ' (__dt_status $last_status)
 end
  
 function fish_prompt --description 'Write out the prompt'
     set -l last_status $status
 
-    if test $COLUMNS -lt 120
-        set -l fish_prompt_pwd_dir_length 1
-    else
-        set -l fish_prompt_pwd_dir_length 0
-    end
+    test $COLUMNS -lt 120; 
+        and set -l fish_prompt_pwd_dir_length 1;
+        or set -l fish_prompt_pwd_dir_length 0
 
     echo (__dt_prompt "$last_status")
     echo -n -s (__dt_suffix "$last_status") ' '
+
     set_color normal
 end
