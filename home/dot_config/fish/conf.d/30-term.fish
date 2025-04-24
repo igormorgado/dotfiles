a181b9a746b4a7c3655c3b16230e6c819464f408
Igor Morgado <morgado.igor@gmail.com>
2025-04-24 00:53:55 -0300

    Update home/.config/fish/conf.d/30-term.fish

--- home/dot_config/fish/conf.d/30-term.fish
+++ home/dot_config/fish/conf.d/30-term.fish
@@ -1,7 +1,6 @@
 test -r $HOME/.dir_colors;
     and command -q dircolors; 
     and set -gx LS_COLORS (dircolors -c $HOME/.dir_colors | sed 's/setenv LS_COLORS //') 
-    # and eval (dircolors -c $HOME/.dir_colors | sed 's/setenv/set -gx/')
 
 set -q DEBUG; and echo -n "Term "; and time_since_last
 
