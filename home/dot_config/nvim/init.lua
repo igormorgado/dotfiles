ddd7eafffe7a043aedf8652cc3578bd5a1e0fe3c
Igor Morgado <morgado.igor@gmail.com>
2025-06-02 12:23:35 -0300

    Update home/.config/nvim/init.lua

--- home/dot_config/nvim/init.lua
+++ home/dot_config/nvim/init.lua
@@ -9,7 +9,7 @@ vim.g.maplocalleader = ','
 vim.g.loaded_ruby_provider = 0
 vim.g.loaded_node_provider = 0
 vim.g.loaded_perl_provider = 0
-vim.g.python3_host_prog = '~/.pyenv/versions/nvim/bin/pytho'
+vim.g.python3_host_prog = '~/.pyenv/versions/nvim/bin/python'
 
 -- Disable netrw (since we're using nvim-tree)
 vim.g.loaded_netrw = 1
