cdd8bc31c6967f39ba60514eabd6bcb464bc6665
Igor Morgado <morgado.igor@gmail.com>
2025-06-30 17:18:24 -0300

    Update home/.config/nvim/init.lua

--- home/dot_config/nvim/init.lua
+++ home/dot_config/nvim/init.lua
@@ -331,7 +331,47 @@ require("lazy").setup({
             "nvim-lua/plenary.nvim"
         },
         config = function()
-            require("claude-code").setup()
+            require("claude-code").setup({
+                window = {
+                    split_ratio = 0.3,
+                    position = "vertical",  -- "botright" | "topleft"
+                    enter_insert = true,
+                    hide_numbers = true,
+                    hide_signcolumn = true,
+                },
+                refresh = {
+                    enable = true,
+                    updatetime = 100,
+                    timer_interval = 100,
+                    show_notifications = true,
+                },
+                git = {
+                    use_git_root = true,
+                },
+                shell = {
+                    separator = '&&',
+                    pushd_cmd = 'pushd',
+                    popd_cmd = 'popd',
+                },
+                command = "claude",
+                command_variants = {
+                    continue = "--continue",
+                    resume = "--resume",
+                    verbose = "--verbose",
+                },
+                keymaps = {
+                    toggle = {
+                        normal = "<C-,>",
+                        terminal = "<C-,>",
+                        variants = {
+                            continue = "<leader>cC",
+                            verbose = "<leader>cV",
+                        },
+                    },
+                    window_navigation = true,
+                    scrolling = true,
+                }
+            })
         end
     },
 
