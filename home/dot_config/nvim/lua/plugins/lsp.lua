76e1a6deda419a595aed751bfe49d734d79a1ff0
Igor Morgado <morgado.igor@gmail.com>
2025-08-25 12:50:05 -0300

    Update .config/nvim/lua/plugins/lsp.lua

--- home/dot_config/nvim/lua/plugins/lsp.lua
+++ home/dot_config/nvim/lua/plugins/lsp.lua
@@ -155,15 +155,15 @@ return {
                 }
             })
 
-            -- Use cmdline & path source for ':'
-            cmp.setup.cmdline(':', {
-                mapping = cmp.mapping.preset.cmdline(),
-                sources = cmp.config.sources({
-                    { name = 'path' }
-                }, {
-                    { name = 'cmdline' }
-                })
-            })
+            -- -- Use cmdline & path source for ':'
+            -- cmp.setup.cmdline(':', {
+            --     mapping = cmp.mapping.preset.cmdline(),
+            --     sources = cmp.config.sources({
+            --         { name = 'path' }
+            --     }, {
+            --         { name = 'cmdline' }
+            --     })
+            -- })
         end
     },
 
