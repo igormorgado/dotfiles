3d43938acec71e050fc2c4538a5ec5e03c2af676
Igor Morgado <morgado.igor@gmail.com>
2025-10-24 11:10:42 -0300

    Update .config/nvim/ftplugin/python.lua

--- home/dot_config/nvim/ftplugin/python.lua
+++ home/dot_config/nvim/ftplugin/python.lua
@@ -17,5 +17,12 @@ vim.opt_local.textwidth = 0
 vim.keymap.set("n", "<localleader>ee", function()
   vim.cmd("update")  -- save file if modified
   local file = vim.fn.shellescape(vim.api.nvim_buf_get_name(0))
-  vim.cmd(("split | terminal python3 %s"):format(file))
+  vim.cmd(("split | terminal python %s"):format(file))
 end, { buffer = true, silent = true, desc = "Execute: Run buffer in split terminal (Python 3)" })
+
+-- Execute current Python file and wait for keypress
+vim.keymap.set('n', '<leader>rr', function()
+    local file = vim.fn.expand('%:p')
+    vim.cmd('write')
+    vim.cmd('vsplit | terminal python "' .. file .. '"; read -p "Press any key to continue..."')
+end, { buffer = true, desc = 'Run current Python file' })
