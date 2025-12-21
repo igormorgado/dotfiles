c012c48701a766289df32af4e29d4e2a1d9159f7
Igor Morgado <morgado.igor@gmail.com>
2025-12-21 14:49:14 -0300

    Update .config/nvim/ftplugin/python.lua

--- home/dot_config/nvim/ftplugin/python.lua
+++ home/dot_config/nvim/ftplugin/python.lua
@@ -26,3 +26,19 @@ vim.keymap.set('n', '<leader>rr', function()
     vim.cmd('write')
     vim.cmd('vsplit | terminal python "' .. file .. '"; read -p "Press any key to continue..."')
 end, { buffer = true, desc = 'Run current Python file' })
+
+-- Folding: use Tree-sitter when folds query exists, otherwise indent
+do
+    local ok_parser, parser = pcall(vim.treesitter.get_parser, 0)
+    local ok_query, folds = pcall(vim.treesitter.query.get, vim.bo.filetype, "folds")
+    if ok_parser and ok_query and folds then
+        vim.opt_local.foldmethod = 'expr'
+        vim.opt_local.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+        pcall(function() parser:parse() end) -- ensure tree exists before first fold calc
+    else
+        vim.opt_local.foldmethod = 'indent'
+    end
+    vim.opt_local.foldenable = true
+    vim.opt_local.foldlevel = 99
+    vim.opt_local.foldlevelstart = 99
+end
