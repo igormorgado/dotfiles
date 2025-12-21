83e98c9921f4ab963fd26205641b2180e5458a80
Igor Morgado <morgado.igor@gmail.com>
2025-12-21 14:54:06 -0300

    Add .config/nvim/ftplugin/tex.lua

---
+++ home/dot_config/nvim/ftplugin/tex.lua
@@ -0,0 +1,8 @@
+vim.opt_local.colorcolumn = "80"
+vim.opt_local.wrapmargin = 1
+vim.opt_local.scrolloff = 5
+vim.opt_local.wrap = false
+vim.opt_local.sidescrolloff = 8
+vim.opt_local.sidescroll = 1
+vim.opt_local.linebreak = true
+vim.opt_local.formatoptions:remove("t")
